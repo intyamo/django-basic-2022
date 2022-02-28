5925966306119166aec2b0f23b288fcce7656cc0
Sergey A <sergey.abramenka@gmail.com>
2022-02-28 17:48:14 +0300

    Imlement URLs with slugs
    Not a breaking change, old urls using record IDs will still work

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Post(models.Model):
@@ -7,9 +8,19 @@ class Post(models.Model):
     text = models.TextField()
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True)
     date = models.DateTimeField(auto_now_add=True, null=True)
+    slug = models.SlugField(max_length=255, null=True, blank=True, unique=True)
 
     def __str__(self):
         return '\n'.join((self.title, f'{self.author.username} | {self.date}', '\n', self.text[:140]))
 
     def get_absolute_url(self):
-        return reverse('single_post', args=[str(self.id)])
+        if self.slug:
+            return reverse('single_post_slug', kwargs={'slug': self.slug})
+        else:
+            return reverse('single_post', args=[str(self.id)])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        return super().save(*args, **kwargs)
--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,7 @@ from .views import AllPostsView, PostView, PostCreateView, PostUpdateView, PostD
 urlpatterns = [
     path('', AllPostsView.as_view(), name='all_posts'),
     path('<int:pk>/', PostView.as_view(), name='single_post'),
+    path('<slug:slug>', PostView.as_view(), name='single_post_slug'),
     path('new/', PostCreateView.as_view(), name='post_create'),
     path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
